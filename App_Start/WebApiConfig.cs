eceb316a68ca4fd969153c9395b9bf505203511d
Subhashree . <subhashreepatra4u@gmail.com>
2025-04-07 22:22:33 -0400

    return only in particular output format from Web API service irrespective of header value

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -20,9 +20,13 @@ namespace EmployeeService
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-            //change Json output with camel casing(FirstName) to pascal casing(firstName)
-            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            /*change Json output with Pascal casing(FirstName) to camel casing(firstName).Can do same for xml as well*/
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            /*return only in Json from Web API service irrespective of header value(ex.if we put Accept:application/xml in header,then also get only Json)*/
+            config.Formatters.Remove(config.Formatters.XmlFormatter); //removed xml ,so only json will return
+
         }
     }
 }
